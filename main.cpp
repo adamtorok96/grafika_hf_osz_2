cb7f055124f10948939bb729a1e0f2b3e0622419
Adam Torok <adamtorok@sch.bme.hu>
2017-11-22 01:08:16 +0100

    so many

--- main.cpp
+++ main.cpp
@@ -29,6 +29,68 @@ const unsigned int windowWidth = 600, windowHeight = 600;
 // OpenGL major and minor versions
 int majorVersion = 3, minorVersion = 3;
 
+void getErrorInfo(unsigned int handle) {
+    int logLen;
+    glGetShaderiv(handle, GL_INFO_LOG_LENGTH, &logLen);
+    if (logLen > 0) {
+        char * log = new char[logLen];
+        int written;
+        glGetShaderInfoLog(handle, logLen, &written, log);
+        printf("Shader log:\n%s", log);
+        delete log;
+    }
+}
+
+// check if shader could be compiled
+void checkShader(unsigned int shader, char * message) {
+    int OK;
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &OK);
+    if (!OK) {
+        printf("%s!\n", message);
+        getErrorInfo(shader);
+    }
+}
+
+// check if shader could be linked
+void checkLinking(unsigned int program) {
+    int OK;
+    glGetProgramiv(program, GL_LINK_STATUS, &OK);
+    if (!OK) {
+        printf("Failed to link shader program!\n");
+        getErrorInfo(program);
+    }
+}
+
+// vertex shader in GLSL
+const char *vertexSource = R"(
+	#version 330
+    precision highp float;
+
+	layout(location = 0) in vec2 vertexPosition;	// Attrib Array 0
+
+	out vec2 texcoord;
+
+	void main() {
+		texcoord = (vertexPosition + vec2(1, 1))/2;							// -1,1 to 0,1
+		gl_Position = vec4(vertexPosition.x, vertexPosition.y, 0, 1); 		// transform to clipping space
+	}
+)";
+
+// fragment shader in GLSL
+const char *fragmentSource = R"(
+	#version 330
+    precision highp float;
+
+	uniform sampler2D textureUnit;
+	in  vec2 texcoord;			// interpolated texture coordinates
+
+	out vec4 fragmentColor;		// output that goes to the raster memory as told by glBindFragDataLocation
+
+	void main() {
+		fragmentColor = texture(textureUnit, texcoord);
+	}
+)";
+
 struct vec3 {
     float x, y, z;
 
@@ -76,6 +138,18 @@ struct vec3 {
         );
     }
 
+    void operator*=(float n) {
+        x *= n;
+        y *= n;
+        z *= n;
+    }
+
+    void operator*=(vec3 const & v) {
+        x *= v.x;
+        y *= v.y;
+        z *= v.z;
+    }
+
 //    operator float*() { return &x; }
 };
 
@@ -104,10 +178,9 @@ public:
     vec3 poz;
     vec3 La;
 
-    Light() {
-        La = vec3(0.280f, 0.330f, 0.980f);
+    Light(vec3 const & pos, vec3 La) : poz(pos), La(La) {
+//        La = vec3(0.280f, 0.330f, 0.980f);
         Lout = vec3(0.9f, 0.9f, 0.9f);
-        poz = vec3(000.0f, 500.0f, 000.0f);
     }
 
     vec3 getLightDir(vec3& intersect) {
@@ -189,7 +262,8 @@ public:
 
         float cosa = fabsf(normal.dot(inDir));
 
-        return (F0 + (vec3(1, 1, 1) - F0) * (float)(pow(1 - cosa, 5))).normalize();
+        // TODO: change it
+        return (F0 + (vec3(1, 1, 1) - F0) * powf(1 - cosa, 5)).normalize();
     }
 
     vec3 shade(vec3 normal, vec3 viewDir, vec3 lightDir, vec3 inRad)
@@ -198,7 +272,7 @@ public:
 
         float cosTheta = normal.dot(lightDir);
 
-        if (cosTheta < 0)
+        if (cosTheta < EPSILON)
             return reflRad;
 
         reflRad = inRad * kd * cosTheta;;
@@ -213,42 +287,50 @@ public:
     }
 };
 
-class GoldMaterial : public Material {
-    public:
-        GoldMaterial() {
-            calcF0(0.17, 3.1, 0.35, 2.7, 1.5, 1.9);
-            ka = vec3(1.0f, 215.0f / 255.0f, 0.0f);
-
-            isReflective = true;
-            isRefractive = false;
-            isDif = true;
-        }
-};
-
 class GlassMaterial : public Material {
 public:
     GlassMaterial() {
         calcF0(1.5, 0, 1.5, 0, 1.5, 0);
-        ka = vec3(0.1, 0.1, 0.1);
+        ka = vec3(0.1, 0.1, 0.1); //
 
         isReflective = true;
         isRefractive = true;
         isDif = false;
 
 //        shininess = 1;
-        n = 1.0f + (float)EPSILON;
+//        n = 1.0f + (float)EPSILON;
+        n = 1.5f;
     }
+};
+
+class GoldMaterial : public Material {
+    public:
+        GoldMaterial() {
+            calcF0(0.17, 3.1, 0.35, 2.7, 1.5, 1.9);
+            ka = vec3(1.0f, 200.0f / 255.0f, 0.0f); //
+
+            isReflective = true;
+        }
 };
 
 class SilverMaterial : public Material {
 public:
     SilverMaterial() {
         calcF0(0.14, 4.1, 0.16, 2.3, 0.13, 3.1);
-        ka = vec3(192.0f / 255.0f, 192.0f / 255.0f, 192.0f / 255.0f);
+//        ka = vec3(192.0f / 255.0f, 192.0f / 255.0f, 192.0f / 255.0f);
+        ka = vec3(0.3f, 0.3f, 0.3f);
 
         isReflective = true;
-        isRefractive = false;
-        isDif = true;
+    }
+};
+
+class CylinderMaterial : public Material {
+public:
+    CylinderMaterial() {
+        ka = vec3(1.0f, 0.0f, 0.0f);
+
+        isReflective = true;
+//        isDif = true;
     }
 };
 
@@ -288,6 +370,7 @@ public:
 
     Hit intersect(Ray & ray) override {
         Hit hit;
+        hit.material = material;
 
         float a = ray.dir.dot(ray.dir);
         float b = 2 * (ray.dir.dot(ray.org - origo));
@@ -304,7 +387,6 @@ public:
             hit.position = ray.org + ray.dir * d;
             hit.t = d;
             hit.normal = (hit.position - origo).normalize();
-            hit.material = material;
 
             return hit;
         }
@@ -325,11 +407,56 @@ public:
         }
 
         hit.normal = (hit.position - origo).normalize();
-        hit.material = material;
 
         return hit;
     }
 
+};
+
+class Cylinder : public Intersectable {
+public:
+    vec3 org;
+    float r;
+
+    Cylinder(float r, Material * material) : Intersectable(material), r(r) {}
+
+    Hit intersect(Ray & ray) {
+        Hit hit;
+        hit.material = material;
+
+        vec3 o = ray.org - org;
+
+        float a = ray.dir.x * ray.dir.x + ray.dir.z * ray.dir.z;
+        float b = 2 * (ray.dir.x * o.x + ray.dir.z * o.z);
+        float c = o.x * o.x + o.z * o.z - r * r;
+
+        float d = b * b - 4 * a * c;
+
+        if( d < 0 )
+            return hit;
+
+        float e = sqrtf(d);
+
+        float t0 = ((-1) * b + e) / (2 * a);
+        float t1 = ((-1) * b - e) / (2 * a);
+
+        float t = t1;
+
+        if( t > t1 )
+            t = t1;
+
+        if( t < 0 )
+            t = t0;
+
+        if( t < 0 )
+            return hit;
+
+        hit.t = t;
+        hit.position = ray.org + ray.dir * t;
+        hit.normal = vec3(hit.position.x * 2, 0, hit.position.z * 2).normalize();
+
+        return hit;
+    }
 };
 
 vec3 rotateX(vec3 v, float angle) {
@@ -376,6 +503,29 @@ public:
         this->v2 = pos + (rotX ? rotateX(v2, 90) : v2);
     }
 
+//    Hit intersect(Ray & ray) {
+//        Hit hit;
+//        hit.material = material;
+//        hit.normal = ((v1 - v0).cross(v2 - v0)).normalize();
+//
+//        float t = ((v0 - ray.org).dot(hit.normal)) / (ray.dir.dot(hit.normal));
+//
+//        if( t < 0 )
+//            return hit;
+//
+//        vec3 p = ray.org + ray.dir * t;
+//
+//        if(
+//                (v1 - v0).cross(p - v0).dot(hit.normal) > 0 &&
+//                (v2 - v1).cross(p - v1).dot(hit.normal) > 0 &&
+//                (v0 - v2).cross(p - v2).dot(hit.normal) > 0
+//                ) {
+//            hit.position = p;
+//            hit.t = t;
+//        }
+//
+//        return hit;
+//    }
     Hit intersect(Ray & ray) {
         Hit hit;
         hit.material = material;
@@ -406,7 +556,7 @@ public:
 
         hit.t = v0v2.dot(qvec) * invDet;
         hit.position = ray.org + ray.dir * hit.t;
-        hit.normal = v0v1.cross(v0v2);
+        hit.normal = (v0v1.cross(v0v2)).normalize();
 
         return hit;
     }
@@ -430,8 +580,8 @@ public:
 
     Ray getRay(int x, int y) {
         vec3 p = lookat +
-                right *((2.0f * (float)x / (float)windowWidth) - 1.0f) +
-                up * (2.0f * (float)y / (float)windowHeight - 1.0f);
+                right   * ((2.0f * (float)x / (float)windowWidth) - 1.0f) +
+                up      * ((2.0f * (float)y / (float)windowHeight) - 1.0f);
 
         return Ray(eye, (p - eye).normalize());
     }
@@ -440,20 +590,25 @@ public:
 
 class World {
 public:
+#define maxLights (3)
 
     Camera camera;
-    Light light;
+    Light * lights[maxLights];
     vec3 La;
 
     Intersectable * objects[200];
-    unsigned nObjects;
+    unsigned nObjects, nLights;
 
-    World() : nObjects(0) {
-        La = light.La;
+    World() : nObjects(0), nLights(0) {
+        La = vec3(0, 0, 0);
     }
 
     void add(Intersectable * obj) {
         objects[nObjects++] = obj;
+    }
+
+    void addLight(Light * light) {
+        lights[nLights++] = light;
     }
 
     void render(vec3 * background) {
@@ -484,19 +639,19 @@ public:
     float sign(vec3 v1, vec3 v2) {
         float dot = v1.dot(v2);
 
-        if (dot < 0.0f - EPSILON)
+        if (dot < 0.0f) // - EPSILON
             return -1.0f;
-        else if (dot > 0.0f + EPSILON)
+        if (dot > 0.0f ) // + EPSILON
             return 1.0f;
-        else
-            return 0.0f;
+
+        return 0.0f;
     }
 
     vec3 trace(Ray & ray, int depth) {
         if (depth > 5)
             return La;
 
-        float EPSZ = 0.07f;
+        float EPSZ = 0.07; // 0.07
 
         Hit hit = firstIntersect(ray);
 
@@ -505,96 +660,53 @@ public:
 
 //        printf("HIT\n");
 
-        vec3 outRadiance = hit.material->ka * La;
+//        vec3 outRadiance = hit.material->ka * La;
+//        vec3 outRadiance = hit.material->ka;
+
+        vec3 outRadiance = hit.material->ka * lights[0]->La;
 
         if (hit.material->isDif) {
-            Ray shadowRay(hit.position + hit.normal*EPSZ*sign(hit.normal, ray.dir*-1.0f), light.Ll(hit.position));
-            Hit shadowHit = firstIntersect(shadowRay);
+            for(int i = 0; i < nLights; i++) {
+                outRadiance =  lights[i]->La * hit.material->ka;
 
-            if ( shadowHit.t < 0 || shadowHit.t > light.getDist(hit.position) )
-                outRadiance = outRadiance + hit.material->shade(hit.normal, (ray.dir*-1.0f), light.Ll(hit.position).normalize(), light.Lout);
+                Ray shadowRay(
+                        hit.position + hit.normal * EPSILON, //EPSZ * sign(hit.normal, ray.dir * -1.0f),
+                        lights[i]->Ll(hit.position)
+                );
+
+                Hit shadowHit = firstIntersect(shadowRay);
+
+                if ( shadowHit.t < 0 || shadowHit.t > lights[i]->getDist(hit.position) ) {
+                    outRadiance = outRadiance + hit.material->shade(hit.normal, (ray.dir*-1.0f), lights[i]->Ll(hit.position), lights[i]->Lout); // .normalize()
+                }
+            }
         }
 
-        if (hit.material->isReflective) {
-            vec3 reflectionDir = hit.material->reflect((ray.dir), hit.normal);
-            Ray reflectedRay(hit.position + hit.normal*EPSZ*sign(hit.normal, (ray.dir*(-1.0f))), reflectionDir);
+        if ( hit.material->isReflective ) {
+            vec3 reflectionDir = hit.material->reflect(ray.dir, hit.normal);
+            Ray reflectedRay(hit.position + hit.normal * sign(hit.normal, (ray.dir* (-1.0f))), reflectionDir); // * EPSZ * sign(hit.normal, (ray.dir* (-1.0f)))
 
-            outRadiance = outRadiance + trace(reflectedRay, depth + 1)*hit.material->Fresnel((ray.dir), hit.normal);
+            outRadiance = outRadiance + trace(reflectedRay, depth + 1) * hit.material->Fresnel((ray.dir), hit.normal);
         }
 
-        if (hit.material->isRefractive) {
+        if ( hit.material->isRefractive ) {
             vec3 refractionDir = hit.material->refract((ray.dir), hit.normal).normalize();
-            Ray refractedRay(hit.position - hit.normal*EPSZ*sign(hit.normal, (ray.dir*(-1.0f))), refractionDir);
+            Ray refractedRay(hit.position - hit.normal * sign(hit.normal, (ray.dir* (-1.0f))), refractionDir); // EPSZ * sign(hit.normal, (ray.dir*(-1.0f)))
             outRadiance = outRadiance + trace(refractedRay, depth + 1)*(vec3(1, 1, 1) - hit.material->Fresnel((ray.dir), hit.normal));
         }
+
+        if( outRadiance.x > 1.0f )
+            outRadiance.x = 1.0f;
+
+        if( outRadiance.y > 1.0f )
+            outRadiance.y = 1.0f;
+
+        if( outRadiance.z > 1.0f )
+            outRadiance.z = 1.0f;
 
         return outRadiance;
     }
 };
-
-
-void getErrorInfo(unsigned int handle) {
-    int logLen;
-    glGetShaderiv(handle, GL_INFO_LOG_LENGTH, &logLen);
-    if (logLen > 0) {
-        char * log = new char[logLen];
-        int written;
-        glGetShaderInfoLog(handle, logLen, &written, log);
-        printf("Shader log:\n%s", log);
-        delete log;
-    }
-}
-
-// check if shader could be compiled
-void checkShader(unsigned int shader, char * message) {
-    int OK;
-    glGetShaderiv(shader, GL_COMPILE_STATUS, &OK);
-    if (!OK) {
-        printf("%s!\n", message);
-        getErrorInfo(shader);
-    }
-}
-
-// check if shader could be linked
-void checkLinking(unsigned int program) {
-    int OK;
-    glGetProgramiv(program, GL_LINK_STATUS, &OK);
-    if (!OK) {
-        printf("Failed to link shader program!\n");
-        getErrorInfo(program);
-    }
-}
-
-// vertex shader in GLSL
-const char *vertexSource = R"(
-	#version 330
-    precision highp float;
-
-	layout(location = 0) in vec2 vertexPosition;	// Attrib Array 0
-
-	out vec2 texcoord;
-
-	void main() {
-		texcoord = (vertexPosition + vec2(1, 1))/2;							// -1,1 to 0,1
-		gl_Position = vec4(vertexPosition.x, vertexPosition.y, 0, 1); 		// transform to clipping space
-	}
-)";
-
-// fragment shader in GLSL
-const char *fragmentSource = R"(
-	#version 330
-    precision highp float;
-
-	uniform sampler2D textureUnit;
-	in  vec2 texcoord;			// interpolated texture coordinates
-
-	out vec4 fragmentColor;		// output that goes to the raster memory as told by glBindFragDataLocation
-
-	void main() {
-		fragmentColor = texture(textureUnit, texcoord); 
-	}
-)";
-
 
 struct vec4 {
     float v[4];
@@ -677,44 +789,28 @@ void generateTorus(World & world, float r, float R, Material * material, vec3 po
 
 // The virtual world: single quad
 FullScreenTexturedQuad fullScreenTexturedQuad;
-
-
 vec3 background[windowWidth * windowHeight];	// The image, which stores the ray tracing result
 
 
 // Initialization, create an OpenGL context
 void onInitialization() {
     glViewport(0, 0, windowWidth, windowHeight);
-    // Ray tracing fills the image called background
-//    for (int x = 0; x < windowWidth; x++) {
-//        for (int y = 0; y < windowHeight; y++) {
-//            background[y * windowWidth + x] = vec3((float)x / windowWidth, (float)y / windowHeight, 0);
-//        }
-//    }
 
     World world;
 
-//    world.add(new Sphere(new GlassMaterial, vec3(0, 0, 1000), 100));
-//    world.add(new Sphere(new GoldMaterial, vec3(150, 10, 10), 50));
-//    world.add(new Sphere(new SilverMaterial, vec3(-150, 10, 10), 50));
-//    float t = 50 * 4;
+//    world.addLight(new Light(vec3(0, 500, 0), vec3(1, 1, 1)));
+    world.addLight(new Light(vec3(0, 0, -250), vec3(1, 1, 1)));
+//    lights[1] = new Light(vec3(0, 0, -250));
+//    lights[2] = new Light(vec3(-250, 30, 0));
 
-    float dist = 0;
-//    world.add(new Sphere(new SilverMaterial, vec3(0, 0, dist), 10));
-//    world.add(new Sphere(new SilverMaterial, vec3(50, 0, dist), 10));
-//    world.add(new Sphere(new SilverMaterial, vec3(0, 50, dist), 10));
+//    world.add(new Sphere(new CylinderMaterial, vec3(0, 10, -250), 10));
+//    world.add(new Sphere(new GlassMaterial, vec3(0, 0, -200), 10));
 
+    generateTorus(world, 20, 70, new GlassMaterial, vec3(0, 0, -100));
+    generateTorus(world, 20, 50, new SilverMaterial, vec3(60, 0, -100), true);
+    generateTorus(world, 20, 50, new GoldMaterial, vec3(-60, 0, -100), true);
 
-//    world.add(new Triangle(
-//            vec3(0, 0, dist),
-//            vec3(0, 50, dist),
-//            vec3(50, 0, dist),
-//            new GlassMaterial()
-//    ));
-
-    generateTorus(world, 40, 140, new GlassMaterial, vec3(0, 0, 100));
-    generateTorus(world, 40, 100, new SilverMaterial, vec3(120, 0, 100), true);
-    generateTorus(world, 40, 100, new GoldMaterial, vec3(-120, 0, 100), true);
+    world.add(new Cylinder(350, new CylinderMaterial));
 
     world.render(background);
 
